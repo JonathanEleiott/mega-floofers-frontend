b15c1683e39c2700c141ae0cd3d7c777fd38506e
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2026-02-19 19:38:11 -0500

    show all bunnies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [allBunnies, setAllBunnies] = useState([]);
+
+  useEffect(() => {
+    const getAllBunnies = async() => {
+      try {
+        const response = await fetch(`/api/v1/bunnies`);
+        console.log(`RESPONSE:`, response);
+        const retrievedBunnies = await response.json();
+        setAllBunnies(retrievedBunnies);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+
+    getAllBunnies();
+  }, [])
 
   return (
     <>
       <h1>Mega Floofers</h1>
+
+      <ul>
+        {
+          allBunnies.map((singleBunny) => {
+            return <li>{singleBunny.name}</li>
+          })
+        }
+      </ul>
     </>
   )
 }
